6bd5f20fc62251f1d76080a010472c8dc1c30ddb
Matheus Gugoni Prada <matheus.prada@sp01.local>
2019-09-19 13:00:56 -0300

    Adicionando método de lista de negociações e protegendo o retorno do método 'get negociacoes'

--- Avancado/client/js/app/controllers/NegociacaoController.js
+++ Avancado/client/js/app/controllers/NegociacaoController.js
@@ -6,20 +6,39 @@ class NegociacaoController {
         this._inputValor = $('#valor');
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
+        this._listanegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        
+        this._listanegociacoes.adiciona(this._criaNegociacao);
+        this._LimpaFormulario();
 
-        let negociacao = new Negociacao(
+        this._listanegociacoes.negociacoes.length = 0;
+
+        console.log(this._listanegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+
+        return new Negociacao(
             DateHelper.textoData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+
+    }
+
+    _LimpaFormulario(){
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
         
-        console.log(negociacao);
-        console.log(DateHelper.dataTexto(negociacao.data));
+        this._inputData.focus();
+
     }
   }
